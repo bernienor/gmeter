a86a0a6b1916523dab86167e7cc9c2fe6ae51aa9
Bernt Hustad Hembre <hembre@omegav.ntun.no>
2016-12-30 10:19:30 +0000

    Got condor protocoll to work wioth both Silent Wings and Condor

--- gmaaler.py
+++ gmaaler.py
@@ -1,72 +1,36 @@
 #!/usr/bin/python3
 import socket
 import meter
-import struct
+import sys
+import simdecode as sim
 
 
-def extract_g(l,protocoll):
-  """
-  >>> extract_g('gforce=1.2',"condor")
-  1.2
-  >>> extract_g('anotervar=12',"condor")
-  
-  """
-  if(protocoll == "condor"):
-    return(unpack_condor(l))
-  if(protocoll == "Silent wings bin"):
-    return(unpack_swb(l))
-  return(0.0)
 
-def unpack_condor(l):
-  parlist = l.split()
-  for param in parlist:
-    mypar = param.decode("utf-8")
-    mypar = mypar.split('=')
-    if('gforce' == mypar[0]):
-      return mypar[1]
-  return 0.0
-  
-def update_g(param, value):
-  if('gforce' == param): 
-      print(value)
 
-def unpack_swb(data):
-  newdata = struct.unpack('Iddfffffffffffffffffffffffffff',data)
-  print((1-newdata[20]/9.81), (1-newdata[21]/9.81), (1-newdata[22]/9.81), "\n") 
-  return(1-newdata[21]/9.81)
-
-def main(argc,argv):
+def main():
  
   UDP_IP = "192.168.0.100"
-  #UDP_PORT = 5005
   UDP_PORT = 55278
 
-  gm = meter.meter(14,15)
-  vario=meter.meter(2,3)
+  gm = meter.meter(17,18)
+#  vario=meter.meter(2,3)
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
   sock.bind((UDP_IP, UDP_PORT))
 
   protocoll = "condor"
-  verbose = false
-  decodeobj = simdecode('condor')
-
+#  protocoll = 'sw_condor'
+#  protocoll = 'sw_bin'
+  decode = sim.simdecode(protocoll)
 
   while True:
-##    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-##    if(protocoll == "condor"):
-##      gm.set(float(extract_g(data, "condor")))
-##    if(protocoll == "swbin"):
-##      gm.set(float(extract_g(data, "Silent wings bin")))
-##    if(protocoll == "swcondor"):
-##      gm.set(float(extract_var(data, "Silent wings condor")))
-##    if(verbose):
-##      print(data)
-
-class simdecode:
-  def __init__ (self, type):
-    if(type=="condor"):
-      parameters = {'gforce'}
-    else
-      raise 'Simulator data format not supported'
-
+    i=0
+    while(i<29): # Needed to speed up things when using condor protocoll on Silent Wings.
+      data, addr = sock.recvfrom(4096)
+      decode.unpack(data)
+      i+=1
+    gm.set(float(decode.get('gforce')))
+#    vario.set(parameters.get('vario'))
+    
+if(__name__ == '__main__'):
+    main()
--- simdecode.py
+++ simdecode.py
@@ -3,43 +3,57 @@
 import struct
 
 
-
-
-
-
 class simdecode:
   def __init__ (self, prot):
     self.protocoll = prot
-    self.pardict.clear()
+    self.pardict = {}
+    self.lookupnames = ('none')
     # Raise exeption for undefined protocoll
 
-    
-def unpack(self,d)
-  if(self.protocoll == "condor"):
-    return(unpack_condor(d))
-  if(self.protocoll == "Silent wings bin"):
-    return(unpack_swb(l))
-  # Raise exeption for undefined protocoll
-  return(0.0)
+  def unpack(self, d):
+    if(self.protocoll == 'condor'):
+      parlist = d.decode("utf-8").split()
+      for param in parlist:
+        mypar = param.split('=')
+        self.pardict[mypar[0]] = mypar[1]
+    elif(self.protocoll == 'sw_condor'):
+       pass
+    elif(self.protocoll == 'sw_bin'):
+       pass
+    else:
+       raise NameError('Unknown protocoll')
+      
+  def get(self, parameter):
+    return self.pardict.get(parameter, 0)
 
-def unpack_condor(self,d):
-  parlist = l.split()
-  self.pardict.clear()
-  for param in parlist:
-    mypar = param.decode("utf-8").split('=')
-    mydict.append(mypar[0],mypar[1])
-  return mylist #encoding ok? 
-  
+
     
-def unpack_swb(self, data):
-  lookupnames = { 'timestamp', 'position_latitude', 'position_longitude',  'altitude_msl',
-                  'altitude_ground', 'altitude_ground_45_inop', 'altitude_ground_forward_inop', 
-                  'pitch', 'yaw', 'd_roll', 'd_pitch', 'd_yaw', 'vx', 'vy',  'vz', 
-                  'vx_wind', 'vy_wind', 'vz_wind',  'v_eas', 'ax', 'ay', 'az', 
-                  'angle_of_attack', 'angle_sideslip', ' vario', 'heading', 
-                  'rate_of_turn', 'airpressure', 'density', 'temperature' }
+"""    
+Unfinished stuff for Silent Wings binary format. 
+  def unpack_swb(self, data):
+    self.lookupnames = ( 'timestamp', 'position_latitude', 'position_longitude',  'altitude_msl',
+                    'altitude_ground', 'altitude_ground_45_inop', 'altitude_ground_forward_inop', 
+                    'pitch', 'yaw', 'd_roll', 'd_pitch', 'd_yaw', 'vx', 'vy',  'vz', 
+                    'vx_wind', 'vy_wind', 'vz_wind',  'v_eas', 'ax', 'ay', 'az', 
+                    'angle_of_attack', 'angle_sideslip', 'vario', 'heading', 
+                    'rate_of_turn', 'airpressure', 'density', 'temperature' )
+      
+    self.data = struct.unpack('Iddfffffffffffffffffffffffffff',data)
+    #self.pardict = zip(lookupnames,data)
+    for name, value in lookupnames, data
+      self.pardict.add(name,value)
+      
+
+
+  def get_variable(self, var):
+    return self.pardict.get(var)
+ 
+
+"""
+
     
-  data = struct.unpack('Iddfffffffffffffffffffffffffff',data)
-  return(zip(lookupnames,data)
-    
+
+
+           
+
 
